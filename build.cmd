ea32d7b1254ca586698139818b4056b05843f17f
Maxim Smirnov <qmbqx8gh@gmail.com>
2022-10-06 11:35:29 +0200

    added chrome extension packaging

--- build.cmd
+++ build.cmd
@@ -1,3 +1,4 @@
 cd src
 del myShips.zip
 "C:\Program Files\7-Zip\7z.exe" a -r myShips.zip myShips
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --pack-extension=c:\src\myShips\src\myShips --pack-extension-key=c:\src\myShips\src\myShips.pem
