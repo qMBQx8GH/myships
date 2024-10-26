aed2e26456fdb47850b1f104d03831cf56f93172
Maxim Smirnov <qmbqx8gh@gmail.com>
2024-10-26 20:15:45 +0200

    fixed utf x0a spaces in ship names

--- game_update.py
+++ game_update.py
@@ -73,10 +73,10 @@ for key, value in d[0].items():
 
     if value.typeinfo.type == 'Ship':
         level = roman.toRoman(value.level)
-        en = (level + ' ' + tr['en'].gettext('IDS_' + value.index)).upper()
-        en_full = (level + ' ' + tr['en'].gettext('IDS_' + value.index + '_FULL')).upper()
-        ru = (level + ' ' + tr['ru'].gettext('IDS_' + value.index)).upper()
-        ru_full = (level + ' ' + tr['ru'].gettext('IDS_' + value.index + '_FULL')).upper()
+        en = (level + ' ' + tr['en'].gettext('IDS_' + value.index)).upper().replace(u'\xa0', ' ')
+        en_full = (level + ' ' + tr['en'].gettext('IDS_' + value.index + '_FULL')).upper().replace(u'\xa0', ' ')
+        ru = (level + ' ' + tr['ru'].gettext('IDS_' + value.index)).upper().replace(u'\xa0', ' ')
+        ru_full = (level + ' ' + tr['ru'].gettext('IDS_' + value.index + '_FULL')).upper().replace(u'\xa0', ' ')
         search = list(set([
             en, en_full, ru, ru_full
         ]))
